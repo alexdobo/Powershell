f972f1deaea0353e0bd663164ddd4ac955086917
alexdobo <alexdobo@users.noreply.github.com>
2017-11-03 08:18:01 -0700

    Timezone
    changed all the times to unix time to make my life easier
    Also had problems with the loop. if there was only one location, $recLocations[$i] would return 'C' instead of the entire string.
    I would like a better solution than this, but it will do for now

--- SendMessagesToBotOnly.ps1
+++ SendMessagesToBotOnly.ps1
@@ -1,5 +1,5 @@
 #Author: Alex Dobrovansky
-#Last updated: 27 Oct 17
+#Last updated: 03 Nov 17
 
 
 
@@ -7,10 +7,9 @@
 $site = "Site 1" #Name of site (becomes the Slack username)
 $date = (Get-Date).AddHours(-2) #How recent to search for
 $size = 50KB #Sorting size
-$recLocations = "C:\Recordings\loc1", "C:\Recordings\loc2" #Location of recordings ("loc1","loc2")
-$colRecLocations = "C:\Call data\loc1","C:\Call Data\loc2" #Location of .col and .rec files.     MUST BE IN SAME ORDER AS ABOVE
-$fileType = ".pv5" #Filetype of recordings
-
+$recLocations = "C:\Documents\WindowsPowerShell\Slack Bot"#, "C:\Recordings\loc2" #Location of recordings ("loc1","loc2")
+$colRecLocations = "C:\WindowsPowerShell\Slack Bot"#,"C:\Call Data\loc2" #Location of .col and .rec files.     MUST BE IN SAME ORDER AS ABOVE
+$fileType = ".mp3"
 
 
 #send function
@@ -29,26 +28,20 @@ function Send-SlackMsg {
 
 
 
-
-
-#get date from col and rec files
-$lastModCol = New-Object System.Collections.ArrayList
-$lastModRec = New-Object System.Collections.ArrayList
-
 #send all of the information to the bot
 #send number of files and last col date s
 
 if ($recLocations.Count -ne $colRecLocations.Count){ #must be same number of reclocs and colRecLocs
-    Throw "The number of Recording locations and .col file location does not match!"
+    Throw "The number of Recording and .col file locations does not match!"
 }else{
-    $botBody=""
-    for ($i=0; $i -lt $recLocations.Count; $i++){
-        
-        #Site,Loc,Large,Small,Empty,Date (UTC),Free Space,Last Col Date,Last Rec Date
-        $botBody += "$site,$($recLocations[$i]),"
-
-        $loc = $recLocations[$i] + "$(get-date -UFormat '\%Y\%m\%d\')"
+    #Site,Loc,Large,Small,Empty,Date (UNIX),Free Space,Last Col Date,Last Rec Date
     
+    if ($recLocations.Count -eq 1){
+        $botBody=""
+        $botBody += "$site,$($recLocations),"
+
+        $loc = $recLocations + "$(get-date -UFormat '\%Y\%m\%d\')"
+        
         #search for files
         $objects = Get-ChildItem -Path $loc -Recurse | Where-Object {$_.Extension -eq $fileType -and $_.LastWriteTime -gt $date}
         $largeObjects = ($objects | Where-Object {$_.Length -gt $size} | Measure-Object).count
@@ -56,21 +49,57 @@ if ($recLocations.Count -ne $colRecLocations.Count){ #must be same number of rec
         $emptyObjects = ($objects | Where-Object {$_.Length -eq 0KB} | Measure-Object).count
 
         #create botmsg
-        $botBody += "$largeObjects,$smallObjects,$emptyObjects,$($date.ToUniversalTime()),"
+        $botBody += "$largeObjects,$smallObjects,$emptyObjects,$([int][double]::Parse((Get-Date ($date).touniversaltime() -UFormat %s)))," #convert to UNIX time
 
         $drive = $loc[0]
         $used,$free = Get-PSDrive $drive | ForEach-Object { $_.Used, $_.Free}
-        $percent = ($used/($used+$free)).ToString("P")
-        $bodyBot += "$percent,"
-
-        $bodyBot += "$((Get-ChildItem -path $colRecLocations[$i] -Recurse | where {$_.Extension -eq ".col"} | sort LastWriteTime -Descending | select -First 1).LastWriteTime.ToUniversalTime()),"
-        $bodyBot += "$((Get-ChildItem -path $colRecLocations[$i] -Recurse | where {$_.Extension -eq ".rec"} | sort LastWriteTime -Descending | select -First 1).LastWriteTime.ToUniversalTime()) `n"
+        $percent = ($used/($used+$free))
+        $botBody += "$percent,"
 
 
+        $lastColTime = (Get-ChildItem -path $colRecLocations -Recurse | where {$_.Extension -eq ".col"} | sort LastWriteTime -Descending | select -First 1).LastWriteTime
+        $lastRecTime = (Get-ChildItem -path $colRecLocations -Recurse | where {$_.Extension -eq ".rec"} | sort LastWriteTime -Descending | select -First 1).LastWriteTime
 
+        $botBody += "$([int][double]::Parse((Get-Date ($lastColTime).ToUniversalTime() -UFormat %s))),"
+        $botBody += "$([int][double]::Parse((Get-Date ($lastRecTime).ToUniversalTime() -UFormat %s)))"
+        $botBody
+        #sends all the data about site in one message
+        Send-SlackMsg -Username $site -Body $botBody -webhookURL "https://hooks.slack.com/services/" # @PS bot
+
+    }else {
+        for ($i=0; $i -lt $recLocations.Count; $i++){
+            $botBody = ""
         
+            $botBody += "$site,$($recLocations[$i]),"
+
+            $loc = $recLocations[$i] + "$(get-date -UFormat '\%Y\%m\%d\')"
+    
+            #search for files
+            $objects = Get-ChildItem -Path $loc -Recurse | Where-Object {$_.Extension -eq $fileType -and $_.LastWriteTime -gt $date}
+            $largeObjects = ($objects | Where-Object {$_.Length -gt $size} | Measure-Object).count
+            $smallObjects = ($objects | Where-Object {$_.Length -lt $size} | Measure-Object).count
+            $emptyObjects = ($objects | Where-Object {$_.Length -eq 0KB} | Measure-Object).count
+
+            #create botmsg
+            $botBody += "$largeObjects,$smallObjects,$emptyObjects,$([int][double]::Parse((Get-Date ($date).touniversaltime() -UFormat %s)))," #convert to UNIX time
+
+            $drive = $loc[0]
+            $used,$free = Get-PSDrive $drive | ForEach-Object { $_.Used, $_.Free}
+            $percent = ($used/($used+$free))
+            $botBody += "$percent,"
+
+
+            $lastColTime = (Get-ChildItem -path $colRecLocations[$i] -Recurse | where {$_.Extension -eq ".col"} | sort LastWriteTime -Descending | select -First 1).LastWriteTime
+            $lastRecTime = (Get-ChildItem -path $colRecLocations[$i] -Recurse | where {$_.Extension -eq ".rec"} | sort LastWriteTime -Descending | select -First 1).LastWriteTime
+
+            $botBody += "$([int][double]::Parse((Get-Date ($lastColTime).ToUniversalTime() -UFormat %s))),"
+            $botBody += "$([int][double]::Parse((Get-Date ($lastRecTime).ToUniversalTime() -UFormat %s)))"
+            $botBody
+            #sends all the data about site in one message
+            Send-SlackMsg -Username $site -Body $botBody -webhookURL "https://hooks.slack.com/services/" # @PS bot
+        }
+
     }
-    #sends all the data about site in one message
-    Send-SlackMsg -Username $site -Body $botBody -webhookURL "https://hooks.slack.com/services/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" # @PS bot
+
 }
 
